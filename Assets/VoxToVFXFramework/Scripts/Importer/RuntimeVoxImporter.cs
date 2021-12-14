847a51e0547c31148825eac75bf203a957413fca
Nicolas Perrier <nperrier@manzalab.com>
2021-12-14 18:38:37 +0100

    Don't write voxels which aren't connected to air

--- Assets/VoxToVFXFramework/Scripts/Importer/RuntimeVoxImporter.cs
+++ Assets/VoxToVFXFramework/Scripts/Importer/RuntimeVoxImporter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -79,8 +80,9 @@ public class RuntimeVoxImporter : MonoBehaviour
     private void WriteVoxelFrameData(VoxelData data, TransformNodeChunk transformNodeChunk)
     {
         FileToVoxCore.Schematics.Tools.Vector3 worldPositionFrame = transformNodeChunk.TranslationAt();
-        Debug.Log(transformNodeChunk.RotationAt());
         Rotation transformRotation = transformNodeChunk.RotationAt();
+        Matrix4x4 matrix4X4 = ReadMatrix4X4FromRotation(transformRotation);
+        Debug.Log(transformNodeChunk.RotationAt() + " " + matrix4X4);
         worldPositionFrame = new FileToVoxCore.Schematics.Tools.Vector3(worldPositionFrame.X - (data.VoxelsDeep / 2), worldPositionFrame.Y - (data.VoxelsWide / 2), worldPositionFrame.Z - (data.VoxelsTall / 2));
         for (int y = 0; y < data.VoxelsTall; y++)
         {
@@ -90,37 +92,96 @@ public class RuntimeVoxImporter : MonoBehaviour
                 {
                     int paletteIndex = data.Get(x, y, z);
                     Color color = paletteIndex == 0 ? Color.Empty : mVoxModel.Palette[paletteIndex - 1];
+
                     if (paletteIndex != 0)
                     {
-                        mVoxels.Add(new VoxelVFX()
+                        Vector3 worldPosition = new Vector3(x + worldPositionFrame.Y, y + worldPositionFrame.Z, z + worldPositionFrame.X);
+                        worldPosition = transformRotation == Rotation._PZ_PX_P ? worldPosition : Quaternion.Euler(0, -90, 0) * worldPosition;
+
+                        bool canAdd = false;
+                        Vector3 finalColor = new Vector3(color.R / (float)255, color.G / (float)255, color.B / (float)255);
+                        if (x - 1 >= 0 && x + 1 < data.VoxelsWide && y - 1 >= 0 && y + 1 < data.VoxelsTall && z - 1 >= 0 && z + 1 < data.VoxelsDeep)
                         {
-                            color = new Vector3(color.R / (float)255, color.G / (float)255, color.B / (float)255),
-                            position = new Vector3(x + worldPositionFrame.Y, y + worldPositionFrame.Z, z + worldPositionFrame.X)
-                        });
+                            int left = data.Get(x - 1, y, z);
+                            int right = data.Get(x + 1, y, z);
+
+                            int top = data.Get(x, y + 1, z);
+                            int bottom = data.Get(x, y - 1, z);
+
+                            int front = data.Get(x, y, z + 1);
+                            int back = data.Get(x, y, z - 1);
+
+                            if (left == 0 || right == 0 || top == 0 || bottom == 0 || front == 0 || back == 0)
+                            {
+                                canAdd = true;
+                            }
+                        }
+                        else
+                        {
+                            canAdd = true;
+                        }
+
+                        if (canAdd)
+                        {
+                            mVoxels.Add(new VoxelVFX()
+                            {
+                                color = finalColor,
+                                position = worldPosition
+                            });
+                        }
                     }
                 }
             }
         }
     }
 
-    public static Quaternion GetTransform(Rotation r, out Vector3 scale)
+    public static Matrix4x4 ReadMatrix4X4FromRotation(Rotation param)
     {
-        byte b = (byte)r;
-        Matrix4x4 m = Matrix4x4.zero;
+        Vector3 scale;
+        byte b = (byte)param;
+        Matrix4x4 matrix4X4 = Matrix4x4.zero;
         int x = b & 3;
         int y = (b >> 2) & 3;
         scale.x = ((b >> 4) & 1) != 0 ? -1 : 1;
         scale.y = ((b >> 5) & 1) != 0 ? -1 : 1;
         scale.z = ((b >> 6) & 1) != 0 ? -1 : 1;
-        m[x, 0] = scale.x;
-        m[y, 1] = scale.y;
-        m[Mathf.Clamp(3 - x - y, 0, 2), 2] = scale.z;
-        m[3, 3] = 1;
-        //Debug.Log($"{r} ({b})↓\r\n{m}");
-        //Debug.Log($"lossyScale={m.lossyScale}");
-        //Debug.Log($"scale={scale}");
-        scale = m.lossyScale;
-        return m.rotation;
+        matrix4X4[x, 0] = scale.x;
+        matrix4X4[y, 1] = scale.y;
+        matrix4X4[Mathf.Clamp(3 - x - y, 0, 2), 2] = scale.z;
+        matrix4X4[3, 3] = 1;
+        return matrix4X4;
+
+        //byte r = Convert.ToByte(param);
+        //int indexRow0 = (r & 3);
+        //int indexRow1 = (r & 12) >> 2;
+        //bool signRow0 = (r & 16) == 0;
+        //bool signRow1 = (r & 32) == 0;
+        //bool signRow2 = (r & 64) == 0;
+
+        //Matrix4x4 result = Matrix4x4.identity;
+        //result.SetRow(0, Vector4.zero);
+        //switch (indexRow0)
+        //{
+        //    case 0: result[0, 0] = signRow0 ? 1f : -1f; break;
+        //    case 1: result[0, 1] = signRow0 ? 1f : -1f; break;
+        //    case 2: result[0, 2] = signRow0 ? 1f : -1f; break;
+        //}
+        //result.SetRow(1, Vector4.zero);
+        //switch (indexRow1)
+        //{
+        //    case 0: result[1, 0] = signRow1 ? 1f : -1f; break;
+        //    case 1: result[1, 1] = signRow1 ? 1f : -1f; break;
+        //    case 2: result[1, 2] = signRow1 ? 1f : -1f; break;
+        //}
+        //result.SetRow(2, Vector4.zero);
+        //switch (indexRow0 + indexRow1)
+        //{
+        //    case 1: result[2, 2] = signRow2 ? 1f : -1f; break;
+        //    case 2: result[2, 1] = signRow2 ? 1f : -1f; break;
+        //    case 3: result[2, 0] = signRow2 ? 1f : -1f; break;
+        //}
+
+        //return result;
     }
 
     private void OnDestroy()
