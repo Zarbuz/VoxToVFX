c556b7fba47d3078c015fd4fb78369d21f4bcb40
Nicolas Perrier <nperrier@manzalab.com>
2022-07-21 12:17:21 +0200

    Implemented the save photo feature

--- Assets/VoxToVFXFramework/Scripts/UI/Photo/PhotoPanel.cs
+++ Assets/VoxToVFXFramework/Scripts/UI/Photo/PhotoPanel.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.IO;
+using SFB;
 using UnityEngine;
 using UnityEngine.UI;
+using VoxToVFXFramework.Scripts.Utils.Extensions;
 
 namespace VoxToVFXFramework.Scripts.UI.Photo
 {
@@ -8,9 +12,17 @@ namespace VoxToVFXFramework.Scripts.UI.Photo
 	{
 		#region ScriptParameters
 
+		[SerializeField] private CanvasGroup BackgroundImage;
+		[SerializeField] private Image PanelBackground;
 		[SerializeField] private Slider SpeedCameraSlider;
 		[SerializeField] private Button SaveButton;
 		[SerializeField] private Button CloseButton;
+
+		#endregion
+
+		#region Fields
+
+		private UnityEngine.Camera mMainCamera;
 
 		#endregion
 
@@ -24,6 +36,8 @@ namespace VoxToVFXFramework.Scripts.UI.Photo
 			CameraManager.Instance.SetCameraState(eCameraState.FREE);
 			CanvasPlayerPCManager.Instance.PauseLockedState = true;
 			SpeedCameraSlider.SetValueWithoutNotify(CameraManager.Instance.SpeedCamera);
+
+			mMainCamera = UnityEngine.Camera.main;
 		}
 
 		private void OnDisable()
@@ -45,7 +59,7 @@ namespace VoxToVFXFramework.Scripts.UI.Photo
 
 		private void OnSaveClicked()
 		{
-
+			StartCoroutine(TakeCaptureCo());
 		}
 
 		private void OnCloseClicked()
@@ -53,6 +67,38 @@ namespace VoxToVFXFramework.Scripts.UI.Photo
 			CameraManager.Instance.SetCameraState(eCameraState.FIRST_PERSON);
 			CanvasPlayerPCManager.Instance.GenericClosePanel();
 			CanvasPlayerPCManager.Instance.PauseLockedState = false;
+		}
+
+		private IEnumerator TakeCaptureCo()
+		{
+			BackgroundImage.alpha = 0.7f;
+			StartCoroutine(BackgroundImage.AlphaFade(0f, .2f));
+			PanelBackground.gameObject.SetActive(false);
+			yield return new WaitForSeconds(0.8f);
+
+			yield return new WaitForEndOfFrame();
+
+			RenderTexture rt = new RenderTexture(Screen.width, Screen.height, 24);
+			mMainCamera.targetTexture = rt;
+			Texture2D screenShot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+			RenderTexture.active = rt;
+			mMainCamera.Render();
+			screenShot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+			mMainCamera.targetTexture = null;
+			screenShot.Apply(false, false);
+			RenderTexture.active = null;
+			Destroy(rt);
+
+			PanelBackground.gameObject.SetActive(true);
+
+			yield return new WaitForEndOfFrame();
+			string path = StandaloneFileBrowser.SaveFilePanel("Save capture", "", "", "png");
+			if (path != null)
+			{
+				byte[] bytes = screenShot.EncodeToPNG();
+				File.WriteAllBytes(path, bytes);
+			}
+
 		}
 
 		#endregion
---
+++ Assets/VoxToVFXFramework/Scripts/Utils/Extensions/ExtensionsCanvasGroup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace VoxToVFXFramework.Scripts.Utils.Extensions
+{
+	public static class ExtensionsCanvasGroup
+	{
+		public static IEnumerator AlphaFade(this CanvasGroup cg, float alpha, float alphaFadeSeconds)
+		{
+			float start = Time.time;
+			while (cg.alpha != alpha)
+			{
+				float elapsed = Time.time - start;
+				float normalisedTime = Mathf.Clamp((elapsed / alphaFadeSeconds) * Time.deltaTime, 0, 1);
+				cg.alpha = Mathf.Lerp(cg.alpha, alpha, normalisedTime);
+				yield return new WaitForEndOfFrame();
+			}
+			yield return true;
+		}
+
+	}
+}
