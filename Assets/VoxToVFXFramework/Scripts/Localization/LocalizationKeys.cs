dbcdbf54b4b7d49dc68b75af0f7988e7f6ec2c8b
Nicolas Perrier <nperrier@manzalab.com>
2022-09-22 11:22:44 +0200

    Fixed incorrect ignore folder

---
+++ Assets/VoxToVFXFramework/Scripts/Localization/LocalizationKeys.cs
@@ -0,0 +1,165 @@
+﻿namespace VoxToVFXFramework.Scripts.Localization
+{
+	public static class LocalizationKeys
+	{
+
+		#region Words
+
+		public const string YES = "[YES]";
+		public const string NO = "[NO]";
+
+		#endregion
+
+		#region ImportScenePanel
+
+		public const string IMPORT_SCENE_TITLE = "[IMPORT_SCENE_TITLE]";
+		public const string IMPORT_SCENE_SUBTITLE = "[IMPORT_SCENE_SUBTITLE]";
+		public const string OPEN_SCENE_TITLE = "[OPEN_SCENE_TITLE]";
+		public const string OPEN_SCENE_SUBTITLE = "[OPEN_SCENE_SUBTITLE]";
+
+		#endregion
+
+		#region Settings
+
+		public const string SETTINGS_KEY_ERROR_ALREADY_SET = "[SETTINGS_KEY_ERROR_ALREADY_SET]";
+		public const string SETTINGS_DATA_CONFIRM_DELETE = "[SETTINGS_DATA_CONFIRM_DELETE]";
+		public const string SETTINGS_WAITING = "[SETTINGS_WAITING]";
+		public const string SETTINGS_WAITING_INPUT = "[SETTINGS_WAITING_INPUT]";
+		public const string SETTINGS_CONFIRM_RESET_ALL = "[SETTINGS_CONFIRM_RESET_ALL]";
+
+
+		#endregion
+
+		#region EditProfile
+
+		public const string EDIT_PROFILE_MISSING_USERNAME = "[EDIT_PROFILE_MISSING_USERNAME]";
+		public const string EDIT_PROFILE_USERNAME_NO_SPACE = "[EDIT_PROFILE_USERNAME_NO_SPACE]";
+		public const string EDIT_PROFILE_UPDATE_SUCCESS = "[EDIT_PROFILE_UPDATE_SUCCESS]";
+		public const string EDIT_PROFILE_UPDATE_FAILED = "[EDIT_PROFILE_UPDATE_FAILED]";
+		public const string EDIT_PROFILE_USERNAME_NOT_AVAILABLE = "[EDIT_PROFILE_USERNAME_NOT_AVAILABLE]";
+		public const string EDIT_PROFILE_INVALID_DISCORD = "[EDIT_PROFILE_INVALID_DISCORD]";
+		public const string EDIT_PROFILE_INVALID_WEBSITE_URL = "[EDIT_PROFILE_INVALID_WEBSITE_URL]";
+		public const string EDIT_PROFILE_INVALID_FACEBOOK_URL = "[EDIT_PROFILE_INVALID_FACEBOOK_URL]";
+		public const string EDIT_PROFILE_INVALID_YOUTUBE_URL = "[EDIT_PROFILE_INVALID_YOUTUBE_URL]";
+		public const string EDIT_PROFILE_INVALID_TIKTOK_URL = "[EDIT_PROFILE_INVALID_TIKTOK_URL]";
+		public const string EDIT_PROFILE_INVALID_SNAPCHAT_URL = "[EDIT_PROFILE_INVALID_SNAPCHAT_URL]";
+		public const string EDIT_PROFILE_INVALID_TWITCH_URL = "[EDIT_PROFILE_INVALID_TWITCH_URL]";
+		public const string EDIT_PROFILE_FILE_TOO_BIG = "[EDIT_PROFILE_FILE_TOO_BIG]";
+
+		#endregion
+
+		#region Profile
+
+		public const string PROFILE_BUY_NOW = "[PROFILE_BUY_NOW]";
+
+		#endregion
+
+		#region Wallet
+
+		public const string WALLET_SIGNING = "[WALLET_SIGNING]";
+
+		#endregion
+
+		#region CollectionPanel
+
+		public const string COLLECTION_HELP_INFO_TEXT = "[COLLECTION_HELP_INFO_TEXT]";
+		public const string COLLECTION_HELP_SMART_CONTRACT = "[COLLECTION_HELP_SMART_CONTRACT]";
+		public const string COLLECTION_EXECUTE_CONTRACT_ERROR = "[COLLECTION_EXECUTE_CONTRACT_ERROR]";
+		public const string COLLECTION_WAITING_CONFIRMATION_TITLE = "[COLLECTION_WAITING_CONFIRMATION_TITLE]";
+		public const string COLLECTION_SMART_CONTRACT_DEPLOYMENT_TITLE = "[COLLECTION_SMART_CONTRACT_DEPLOYMENT_TITLE]";
+
+
+		#endregion
+
+		#region Creation
+
+		public const string CREATION_UPLOAD_ERROR = "[CREATION_UPLOAD_ERROR]";
+		public const string CREATION_MINT_NAME_TOO_SHORT = "[CREATION_MINT_NAME_TOO_SHORT]";
+		public const string CREATION_MINT_MISSING_IMAGE = "[CREATION_MINT_MISSING_IMAGE]";
+
+
+		#endregion
+
+		#region Preview
+
+		public const string PREVIEW_MISSING_TITLE = "[PREVIEW_MISSING_TITLE]";
+		public const string PREVIEW_MISSING_DESCRIPTION = "[PREVIEW_MISSING_DESCRIPTION]";
+
+		#endregion
+
+		#region Minted
+
+		public const string MINTED_ON_DATE = "[MINTED_ON_DATE]";
+		public const string MINT_IN_PROGRESS_TITLE = "[MINT_IN_PROGRESS_TITLE]";
+		public const string MINT_IN_PROGRESS_DESCRIPTION = "[MINT_IN_PROGRESS_DESCRIPTION]";
+		#endregion
+
+		#region Loading
+
+		public const string LOADING_SCENE_DESCRIPTION = "[LOADING_SCENE_DESCRIPTION]";
+		public const string LOADING_DOWNLOAD_MODEL = "[LOADING_DOWNLOAD_MODEL]";
+
+		#endregion
+
+		#region SetBuyPrice
+
+		public const string SET_BUY_AMOUNT_REQUIRED = "[SET_BUY_AMOUNT_REQUIRED]";
+		public const string SET_BUY_AT_LEAST_0_01ETH = "[SET_BUY_AT_LEAST_0_01ETH]";
+		public const string SET_BUY_PRICE = "[SET_BUY_PRICE]";
+		public const string SET_BUY_PRICE_TITLE = "[SET_BUY_PRICE_TITLE]";
+		public const string SET_BUY_PRICE_DESCRIPTION = "[SET_BUY_PRICE_DESCRIPTION]";
+		public const string SET_BUY_PRICE_WAITING_TITLE = "[SET_BUY_PRICE_WAITING_TITLE]";
+		public const string SET_BUY_PRICE_WAITING_DESCRIPTION = "[SET_BUY_PRICE_WAITING_DESCRIPTION]";
+		public const string SET_BUY_PRICE_SUCCESS_DESCRIPTION = "[SET_BUY_PRICE_SUCCESS_DESCRIPTION]";
+		public const string CHANGE_BUY_NOW_PRICE_TITLE = "[CHANGE_BUY_NOW_PRICE_TITLE]";
+		public const string SET_BUY_PRICE_SUCCESS_TITLE = "[SET_BUY_PRICE_SUCCESS_TITLE]";
+		#endregion
+
+		#region RemoveBuyPrice
+
+		public const string REMOVE_BUY_NOW_WAITING_PRICE_TITLE = "[REMOVE_BUY_NOW_WAITING_PRICE_TITLE]";
+		public const string REMOVE_BUY_NOW_WAITING_PRICE_DESCRIPTION = "[REMOVE_BUY_NOW_WAITING_PRICE_DESCRIPTION]";
+		public const string REMOVE_BUY_NOW_REMOVED_TITLE = "[REMOVE_BUY_NOW_REMOVED_TITLE]";
+		public const string REMOVE_BUY_NOW_REMOVED_DESCRIPTION = "[REMOVE_BUY_NOW_REMOVED_DESCRIPTION]";
+
+
+		#endregion
+
+		#region Transfer
+
+		public const string TRANSFER_NFT = "[TRANSFER_NFT]";
+		public const string TRANSFER_NFT_DESCRIPTION = "[TRANSFER_NFT_DESCRIPTION]";
+		public const string TRANSFER_NFT_ADDRESS_MANDATORY = "[TRANSFER_NFT_ADDRESS_MANDATORY]";
+		public const string TRANSFER_NFT_ADDRESS_INVALID = "[TRANSFER_NFT_ADDRESS_INVALID]";
+		public const string TRANSFER_NFT_WAITING_TITLE = "[TRANSFER_NFT_WAITING_TITLE]";
+		public const string TRANSFER_NFT_WAITING_DESCRIPTION = "[TRANSFER_NFT_WAITING_DESCRIPTION]";
+		public const string TRANSFER_NFT_SUCCESS_TITLE = "[TRANSFER_NFT_SUCCESS_TITLE]";
+		public const string TRANSFER_NFT_SUCCESS_DESCRIPTION = "[TRANSFER_NFT_SUCCESS_DESCRIPTION]";
+
+		#endregion
+
+		#region BurnNFT
+
+		public const string BURN_NFT_WAITING_TITLE = "[BURN_NFT_WAITING_TITLE]";
+		public const string BURN_NFT_WAITING_DESCRIPTION = "[BURN_NFT_WAITING_DESCRIPTION]";
+		public const string BURN_NFT_SUCCESS_TITLE = "[BURN_NFT_SUCCESS_TITLE]";
+		public const string BURN_NFT_SUCCESS_DESCRIPTION = "[BURN_NFT_SUCCESS_DESCRIPTION]";
+
+		#endregion
+
+		#region BurnCollection
+
+		public const string BURN_COLLECTION_WAITING_TITLE = "[BURN_COLLECTION_WAITING_TITLE]";
+		public const string BURN_COLLECTION_WAITING_DESCRIPTION = "[BURN_COLLECTION_WAITING_DESCRIPTION]";
+
+		#endregion
+
+		#region Follow
+
+		public const string FOLLOW = "[FOLLOW]";
+		public const string UNFOLLOW = "[UNFOLLOW]";
+		public const string FOLLOWING = "[FOLLOWING]";
+
+		#endregion
+	}
+}
